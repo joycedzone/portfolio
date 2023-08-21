6d0618cb24f0d1311e951ee160affee11e109afd
Joyce Dizon <joyce.dizon@globe.com.ph>
2023-08-22 01:55:43 +0800

    added animation

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -19,7 +19,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    onSameUrlNavigation: 'reload',
+    scrollPositionRestoration: 'enabled'
+
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -14,11 +14,42 @@ export class HeaderComponent implements OnInit {
 
   showMenu: boolean = false;
 
+  headerItems = [
+    {
+      title: 'About',
+      url: '/about',
+      fragment: null,
+      icon: '../../assets/about.svg'
+    },
+    {
+      title: 'Portfolio',
+      url: '/',
+      fragment: 'portfolio',
+      icon: '../../assets/portfolio.svg'
+    },
+    {
+      title: 'Skills',
+      url: '/about',
+      fragment: 'skills',
+      icon: '../../assets/skill.svg'
+    },
+    {
+      title: 'Contact',
+      url: '/',
+      fragment: 'contact',
+      icon: '../../assets/contact.svg'
+    }
+  ]
+
   ngOnInit(): void {
   }
 
-  redirect(url: string) {
-    this.route.navigate([url])
+  redirect(url: string, fragment?: string | null) {
+    if (fragment) {
+      this.route.navigate([url], { fragment: fragment })
+    } else {
+      this.route.navigate([url])
+    }
     this.showMenu = false;
   }
 
