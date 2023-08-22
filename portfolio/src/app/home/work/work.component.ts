804b7d4c6d693cdabd7ad81bb42497ce604eb14a
Joyce Dizon <joyce.dizon@globe.com.ph>
2023-08-22 19:25:24 +0800

    Updated content

--- portfolio/src/app/home/work/work.component.ts
+++ portfolio/src/app/home/work/work.component.ts
@@ -9,38 +9,38 @@ export class WorkComponent {
 
   showOptions = false;
   title: string = "Portfolio"
-  desc: string = "This is a showcase of my best work in a variety of fields including Graphic and Web Design, No-Code Development, Product Design and Product Management.The world of digital design and development is constantly evolving and so has my role over the last 15 years. I'm still learning and gaining new skills every day.";
+  desc: string = "Throughout my journey as a Software Developer, I've had the privilege of working on a diverse array of projects. My portfolio showcases a selection of these ventures, each of which has challenged me to push my boundaries and grow both professionally and personally. From large-scale enterprise applications to small-scale passion projects, each endeavor has contributed to my growth as a developer.";
   role: string = '';
   responsibility: string = '';
   url: string = ''
   projects = [
     {
       title: 'Portfolio',
-      desc: "A web application developed thru Angular. This application showcases the developer's best work. A web application developed thru Angular. This application showcases the developer’s best work",
+      desc: "The Portfolio is a platform dedicated to showcasing my career milestones and expertise in an interactive and visually appealing manner. It's not just a static CV; it's a dynamic space where visitors can gain insights into my professional background, projects, and the value I bring to the table.",
       role: 'Front-end Developer',
       responsibility: 'Design, implementation, deployment',
-      url: 'github.com.ph'
+      url: 'https://github.com/joycedzone/portfolio'
     },
     {
-      title: 'Chatbot',
-      desc: 'A conversational interface solution that provides automated customer service for different businesses via chatbot.',
+      title: 'TelcoChat',
+      desc: 'TelcoChat is a next-generation chatbot designed to enhance customer experiences within the telecommunications domain. With an array of advanced features, it streamlines customer inquiries, troubleshoots issues, and offers real-time assistance. Whether you need help with billing, technical support, or service activation, TelcoChat is here to provide instant solutions.',
       role: 'Developer, Development Lead',
       responsibility: 'Solution, implementation, deployment',
-      url: 'github.com.ph'
+      url: ''
     },
     {
       title: 'ARCS',
-      desc: 'Automated Recruitment and Crewing System. A system used for recruitment. Consists of three web applications: (1) Application Portal, (2) Back-office Portal, adn (3) Exam Portal',
+      desc: 'ARCS is an integrated solution designed to streamline the recruitment process and crew management while introducing an innovative assessment approach. From submitting CVs to conducting exams, this system ensures a seamless experience for both applicants and recruiters.',
       role: 'Back-end Developer (API)',
       responsibility: 'Solution, implementation, deployment ',
-      url: 'github.com.ph'
+      url: ''
     },
     {
-      title: 'Digital Shop',
-      desc: 'A web application that allows users to create account transactions: acquisition, renewal, reloading, etc.',
+      title: 'TelcoShop',
+      desc: 'TelcoShop redefines the way customers engage with telecom services. It offers an intuitive and convenient interface for users to manage their mobile services, purchase products, and stay connected effortlessly.',
       role: 'Full-stack Developer',
       responsibility: 'Solution, implementation, deployment ',
-      url: 'github.com.ph'
+      url: ''
     }
   ];
 
